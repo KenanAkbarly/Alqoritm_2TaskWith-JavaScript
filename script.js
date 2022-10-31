18164efe17407c4865ae6a2be061b2646967b8ec
KenanAkbarli <tu6m5m78u@code.edu.az>
2022-11-01 00:47:06 +0400

    feat:add algorithm2 task

---
+++ script.js
@@ -0,0 +1,18 @@
+// Sum Numbers
+/*let sum = 0;
+let getNumber = -5;
+if(getNumber < 0){
+    getNumber*=-1;
+}
+for(let i = 1; i<=getNumber ; i++){
+    sum = sum + i;
+}
+console.log(sum);*/
+
+
+//Taken Number of elementsin loop
+for (let i = 1; i<100;i++){
+    if (i % 3 === 0 || i % 5 === 0){
+        console.log(i);
+}
+}
